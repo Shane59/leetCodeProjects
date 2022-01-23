7e22fc40811313741b95ba9455b37e0fa6f22f8d
Shinya Aoi <saoi@Shinyas-MacBook-Pro.local>
2022-01-23 11:21:41 +0900

    improve min-stack

--- done/easy-problems/155-minStack.java
+++ done/easy-problems/155-minStack.java
@@ -1,48 +1,47 @@
 class MinStack {
-
-  /** initialize your data structure here. */
-  Stack<Integer> normalStack = new Stack<>();
-  Stack<Integer> minStack = new Stack<>();
-  public MinStack() {
-      
-  }
-  
-  public void push(int x) {
-      normalStack.push(x);
-      if (!minStack.isEmpty()) {
-          int minNum = minStack.peek();
-          if (x <= minNum) {
-              minStack.push(x);
-          }
-      } else {
-          minStack.push(x);
-      }
-  }
-  
-  public void pop() {
-      int x = normalStack.pop();
-      if (!minStack.isEmpty()) {
-          int minNum = minStack.peek();
-          if (x == minNum) {
-              minStack.pop();
-          }
-      }
-  }
-  
-  public int top() {
-      return normalStack.peek();
-  }
-  
-  public int getMin() {
-      return minStack.peek();
-  }
+    List<Integer> stack;
+    List<Integer> minStack;
+    public static int minNum;
+    public MinStack() {
+      stack = new LinkedList<>();
+      minStack = new LinkedList<>();
+    }
+    
+    public void push(int val) {
+        if (minStack.size() == 0) {
+            minStack.add(val);
+        } else {
+            int currSmallest = minStack.get(minStack.size() - 1);
+            if (val <= currSmallest) {
+                minStack.add(val);
+            }
+        }
+        stack.add(val);
+    }
+    
+    public void pop() {
+        int removed = stack.get(stack.size() - 1);
+        int smallest = minStack.get(minStack.size() - 1);
+        if (removed == smallest) {
+            minStack.remove(minStack.size() - 1);
+        }
+        stack.remove(stack.size() - 1);
+    }
+    
+    public int top() {
+        return stack.get(stack.size() - 1);
+    }
+    
+    public int getMin() {
+        return minStack.get(minStack.size() - 1);
+    }
 }
 
 /**
-* Your MinStack object will be instantiated and called as such:
-* MinStack obj = new MinStack();
-* obj.push(x);
-* obj.pop();
-* int param_3 = obj.top();
-* int param_4 = obj.getMin();
-*/
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
