2968bdac6ee73a2998b1883ffaf0dbcba603f54b
Shinya Aoi <saoi@Shinyas-MacBook-Pro.local>
2022-02-12 23:28:35 +0900

    improve 283

--- done/easy-problems/283-moveZeros.java
+++ done/easy-problems/283-moveZeros.java
@@ -1,21 +1,19 @@
 class Solution {
-  public void moveZeroes(int[] nums) {
-      if(nums.length>1) {
-          int last = nums.length-1;
-          for(int i=0; i<nums.length;){
-              if(nums[i]==0) {
-                  int index = i;
-                  while(index+1<nums.length){
-                      int temp = nums[index+1];
-                      nums[index] = temp;
-                      index++;
-                  }
-                  nums[index] = 0;
-                  last--;
-              }
-          if(nums[i] != 0) i++;
-          if(i >= last) break;
-          }
-      } 
-  }
+    public void moveZeroes(int[] nums) {
+        int base = 0;
+        int i = 0;
+        
+        while (base < nums.length) {
+            if (i < nums.length) {
+                if (nums[i] == 0) {
+                    i++;
+                } else {
+                    int temp = nums[base];
+                    nums[base++] = nums[i++];
+                }
+            } else {
+                nums[base++] = 0;
+            }
+        }
+    }
 }
